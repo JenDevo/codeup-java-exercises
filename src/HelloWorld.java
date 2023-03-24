421b40812227f83c98349fef8dde1891b4518c72
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-24 14:29:59 -0500

    Completed Codeup Java Syntax, Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,20 +3,27 @@ public class HelloWorld {
         System.out.print("Hello, World!\n");
         /* Added a new line above so that when favorite number was printed it started on new line */
 
+        /* SYNTAX< TYPES< VARIABLES: Question 1 */
         int myFavoriteNumber = 16;
                 System.out.println(myFavoriteNumber);
 
+        /* Question 2 */
         String myString = "Hello there from this new string!";
                 System.out.println(myString);
 
-                myString = "H";
+        /* Question 3 - string doesn't take in char*/
+                /** myString = 'H';
                 System.out.println(myString);
+                */
 
-                myString = "3.14159";
+        /* Question 4 - string doesn't work w numbers */
+                /**myString = 3.14159;
                 System.out.println(myString);
+                */
 
         System.out.println("======spacing=======");
 
+        /* Question 5-9 */
         double myNumber = 3.14;
         System.out.println(myNumber);
 
@@ -29,6 +36,7 @@ public class HelloWorld {
 
         System.out.println("======spacing=======");
 
+        /* Question 10 - Increment and decrement */
         int x = 5;
         System.out.println(x++);
         /* above x is incremented after the variable so output=5 until x called again */
@@ -41,6 +49,7 @@ public class HelloWorld {
 
         System.out.println("======spacing=======");
 
+        /* Question 12 */
        /** String theNumberThree = "three";
         * Object o = theNumberThree;
         * int three = (int) o;
@@ -48,7 +57,7 @@ public class HelloWorld {
        /* int three = (int) "three"; */
         /* ERROR for both commented out above I believe due to int and string not correlating*/
 
-
+        /* Question 13 */
         /* Shorthand Assignment Operators */
         int x3 = 4;
         x3 += 5;
@@ -70,5 +79,16 @@ public class HelloWorld {
 
         System.out.println("======spacing=======");
 
+        /* Question 14 */
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println("======spacing=======");
+
+        /*BONUS*/
+        int myInt = 42;
+        Integer myInteger = 42;
+        System.out.println(myInt);
+        System.out.println(myInteger);
     }
 }
