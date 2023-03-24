4167deb13247e1438fe593672fdc0fe651400794
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-24 16:11:17 -0500

    Finished first Console IO exercise section

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,10 @@
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+/* BELOW: is creating a string to output pi using the same variable without manipulating it to have a string */
+        System.out.format("The value of pi is approximately %s", pi);
+
+    }
+}
