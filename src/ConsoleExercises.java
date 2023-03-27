64c4b8b0ae4ac7d4f783098ca382fc17925fa81a
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-27 11:49:05 -0500

    Still working on console IO exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -10,19 +10,20 @@ public class ConsoleExercises {
 
         System.out.println("========SPACING==========");
 
-        /* Exploring SCANNER CLASS */
+
+
+        /* EXPLORING SCANNER CLASS */
         Scanner userInput = new Scanner(System.in);
             /*Question 1*/
-//        System.out.print("What is your favorite number?");
-//                int userInt = userInput.nextInt();
-//                System.out.println(userInt);
+        System.out.print("What is your favorite number?");
+                int userInt = userInput.nextInt();
+                System.out.println(userInt);
 
-//        System.out.print("What is your favorite color?");
-//                System.out.println(userInput.next());
+        System.out.print("What is your favorite color?");
+                System.out.println(userInput.next());
                 /* ABOVE: is a scanner user to get user input and return user input */
 
             /*Question 2*/
-
         System.out.println("What are your 3 favorite words?");
                 String firstWord = userInput.next();
                 String secondWord = userInput.next();
@@ -31,4 +32,7 @@ public class ConsoleExercises {
             System.out.println("Your Second favorite word is: " + secondWord);
             System.out.println("Your Third favorite word is: " + thirdWord);
     }
+
+            /* Question 3 */
+
 }
