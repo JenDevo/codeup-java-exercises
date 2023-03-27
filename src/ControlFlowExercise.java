db41378ce8b44c59debc6cbdb6efc1051edd3d3c
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-27 12:06:02 -0500

    Made while loop, onto do while

---
+++ src/ControlFlowExercise.java
@@ -0,0 +1,19 @@
+public class ControlFlowExercise {
+    public static void main(String[] args){
+
+        /* Loop basics */
+
+        /*(A) While-loop: Displays in console numbers 5-15 with "i is #" ex:"i is 5"*/
+       /*  int i = 5;
+        *       while (i <= 15){
+        *           System.out.println("i is " + i);
+        *           i++;
+        *       }
+        */
+
+        /* Fizzbuzz */
+
+
+        /* Display a table of powers */
+    }
+}
