a2e426f9d01da5785e506e6c1097dd7f19d1f74f
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-27 17:31:35 -0500

    Fizzbuzz complete

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -33,13 +33,16 @@ public class ControlFlowExercise {
 
         /* Fizzbuzz */
 
-        for(int i = 1; i <= 100; i++){
-            if(i % 3 == 0){
-                System.out.println("Fizz");
-            }else if (i % 5 == 0){
-             System.out.println("Buzz");
-            }else if ((i % 3 == 0) && (i % 5 == 0)){
+        for(int i = 1; i < 100; i++){
+            boolean three = (i % 3) == 0;
+            boolean five = (i % 5) == 0;
+
+            if(three && five){
                 System.out.println("FizzBuzz");
+            }else if (three){
+             System.out.println("Fizz");
+            }else if (five){
+                System.out.println("Buzz");
             }else {
                 System.out.println(i);
             }
