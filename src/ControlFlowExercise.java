b78319f7934bd4e9ab59429de8a22e29210396be
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-27 15:59:54 -0500

    Finished exercise 1 onto bob

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -19,22 +19,31 @@ public class ControlFlowExercise {
         */
 
         /* (B) DO-WHILE: Count by 2's, start @zero and end @100 */
-          /*    long i = 2;
+        /*    long i = 2;
             *       do {
             *           System.out.println(i);
             *           i = i * i;
             *       } while (i < 1000000);
-        */
 
-        /* FOR LOOP REFACTOR for above*/
-
-        for (long i = 2; i < 1000000; i=i*i){
-            System.out.println(i);
-        }
-
+         * FOR LOOP REFACTOR for above
+            *       for (long i = 2; i < 1000000; i=i*i){
+            *       System.out.println(i);
+            *       }
+            */
 
         /* Fizzbuzz */
 
+        for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else if (i % 5 == 0){
+             System.out.println("Buzz");
+            }else if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("FizzBuzz");
+            }else {
+                System.out.println(i);
+            }
+        }
 
         /* Display a table of powers */
     }
---
+++ src/StringExercise.java
@@ -0,0 +1,23 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        /*   String weDontNeedNo = ("We don't need no education.");
+         *         weDontNeedNo = weDontNeedNo + " We don't need no thought control!";
+         *         System.out.println(weDontNeedNo);
+         *      }
+         */
+
+       /*   String checkThis = "Check \"this\" out!, \"s inside of \"s!\n";
+        *        System.out.println(checkThis);
+        *    }
+        */
+
+        /* String mainDrive = "In windows, the main drive is usually C:\\\n";
+          *  System.out.println(mainDrive);
+         */
+
+        String backSlash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!\n";
+        System.out.println("backSlash = " + backSlash);
+    }
+}
