42ef0a8a59f955dbbf61bd9d288502f3842e528e
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-27 14:01:14 -0500

    Refactored first exercies as for loop

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -9,15 +9,28 @@ public class ControlFlowExercise {
         *           System.out.println("i is " + i);
         *           i++;
         *       }
+        *
+        *
+        * SAME AS ABOVE BUT IN FOR LOOP
+        *
+        * for(int i = 5; i <=15; i++) {
+            System.out.println("i is: " + i);
+        }
         */
 
         /* (B) DO-WHILE: Count by 2's, start @zero and end @100 */
-           long i = 2;
-               do {
-                   System.out.println(i);
-                   i = i * i;
+          /*    long i = 2;
+            *       do {
+            *           System.out.println(i);
+            *           i = i * i;
+            *       } while (i < 1000000);
+        */
 
-               } while (i < 1000000);
+        /* FOR LOOP REFACTOR for above*/
+
+        for(int i = 5; i <=15; i++){
+            System.out.println("i is: " + i);
+        }
 
 
         /* Fizzbuzz */
