0f82d5208116c62ea9d27de5fb2f0867408c31ef
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-30 15:02:02 -0500

    finsihed understanding references

--- src/Person.java
+++ src/Person.java
@@ -10,24 +10,32 @@ public class Person {
 
     public String getName(){
 //TODO: return the person's name
-        return this.name;
+        return name;
     }
 
     public void setName(String name){
 //TODO: change the name field to the passed value
         this.name = name;
     }
-    public void sayHello(){
+    public String sayHello(){
 //TODO: print a message to the console using the person's name
-   System.out.println("Hello " + this.name + "!");
+   return String.format("Hello " + this.name + "!");
     }
 
     /* Test in console */
     public static void main(String[] args){
         Person Person1 = new Person("Jenna");
+        Person student = new Person ("Jacob");
+        Person student2 = new Person("Anna");
 
         String Person1Name = Person1.getName();
+        String newStudent = student.getName();
+        String secondNewStudent = student2.getName();
+        System.out.println("The name of the student is " + newStudent);
         System.out.println("The name of the first person is " + Person1Name);
+        System.out.println(Person1.sayHello());
+        System.out.println("We have a new student, their name is " + secondNewStudent);
+        System.out.println(student2.sayHello());
     }
 
 }
