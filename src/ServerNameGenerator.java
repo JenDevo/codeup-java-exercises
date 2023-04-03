6f3088756aa192b321e8f0be679b59560d27cd0d
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-04-03 10:18:30 -0500

    Finished server name generator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+
+    public static String randomName(String[] arr){
+        Random randomWord = new Random();
+        int wordIndex = randomWord.nextInt(arr.length);
+        return arr[wordIndex];
+    }
+
+
+    public static void main(String[] arr){
+        String[] adjectives = {"busy", "cautious", "adventurous", "calm", "hilarious", "grotesque", "helpful", "kind", "precious", "wild"};
+        String[] nouns = {"law", "nature", "idea", "temperature", "power", "language", "security", "movie", "organization", "teacher"};
+
+
+        String randomAdjective = randomName(adjectives);
+        String randomNoun = randomName(nouns);
+
+        System.out.println("Here is your server name: ");
+        System.out.printf("%s %s", randomAdjective, randomNoun);
+    }
+
+
+
+}
