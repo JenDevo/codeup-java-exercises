19b47d7d1d1e73d97f24f21081f7ac3f39c82a86
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-04-10 11:02:38 -0500

    Completed exceptions and error handling exercises

--- src/util/Input.java
+++ src/util/Input.java
@@ -10,6 +10,7 @@ public class Input {
     }
 
     public String getString(){
+
         System.out.println("Enter string\n");
         return scanner.nextLine();
     }
@@ -18,33 +19,48 @@ public class Input {
         return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
         }
 
+
+        /* Refactored for Exceptions and Error handling exercise */
     int getInt(){
-        return scanner.nextInt();
+            try {
+                int intInput = Integer.valueOf(getString());
+                return intInput;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. " + e);
+                return 0;
+            }
+        }
+
+//    public int getInt(int min, int max){
+//        int userInt;
+//        do{
+//            System.out.printf("Enter a whole number between %d and %d \n", min, max);
+//            userInt = scanner.nextInt();
+//        }while (userInt > max || userInt < min);
+//        return userInt;
+//    }
+
+    /* Refactored for Exceptions and Error handling exercise */
+    public double getDouble() {
+        try {
+            double doubleInt = Double.valueOf(getString());
+            return doubleInt;
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. " + e);
+            return 0;
+        }
+
+
+//    double getDouble(double min, double max){
+//        Double userDouble;
+//        do{
+//            System.out.printf("Enter a decimal number between %d and %d\n", min, max);
+//            userDouble = scanner.nextDouble();
+//        }while (userDouble > max || userDouble < min);
+//        return userDouble;
+//    }
+
+
     }
-
-    int getInt(int min, int max){
-        int userInt;
-        do{
-            System.out.printf("Enter a whole number between %d and %d \n", min, max);
-            userInt = scanner.nextInt();
-        }while (userInt > max || userInt < min);
-        return userInt;
-    }
-
-    public double getDouble(){
-        return scanner.nextDouble();
-    }
-
-    double getDouble(double min, double max){
-        Double userDouble;
-        do{
-            System.out.printf("Enter a decimal number between %d and %d\n", min, max);
-            userDouble = scanner.nextDouble();
-        }while (userDouble > max || userDouble < min);
-        return userDouble;
-    }
-
-
-
 }
 
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,20 +3,20 @@ package util;
 public class InputTest {
     public static void main(String[] args){
         Input input = new Input();
-        String myString = input.getString();
-        System.out.println("You entered: " + myString);
+//        String myString = input.getString();
+//        System.out.println("You entered: " + myString);
 
-        System.out.println("Is cold weather your favorite type of weather?\n");
-        boolean coldWeather = input.yesNo();
-        System.out.println("Cold weather? = " + coldWeather);
+//        System.out.println("Is cold weather your favorite type of weather?\n");
+//        boolean coldWeather = input.yesNo();
+//        System.out.println("Cold weather? = " + coldWeather);
 
         System.out.println("Enter a whole number.");
         int myInt = input.getInt();
         System.out.println("Your whole number = " + myInt);
 
 
-        int myInteger = input.getInt(16, 35);
-        System.out.println("You picked the integer " + myInteger);
+//        int myInteger = input.getInt(16, 35);
+//        System.out.println("You picked the integer " + myInteger);
 
         System.out.println("Enter a decimal number ");
         double myDouble = input.getDouble();
